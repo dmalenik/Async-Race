78f46a266e17bcbb71206f3e281652d3da13a383
Dmitriy Malenik <dmalenik@gmail.com>
2023-01-12 19:58:11 +0100

    'feat: add toggler to views'

---
+++ frontend/setViewToggler.js
@@ -0,0 +1,30 @@
+const setViewToggler = () => {
+    const toggler = document.createElement('button')
+
+    toggler.setAttribute('id', 'toggler')
+    toggler.setAttribute('type', 'button')
+
+    toggler.innerText = 'Switch'
+
+    toggler.addEventListener('click', (event) => {
+        if (document.querySelector('#garage').classList.contains('f-col')) {
+            document
+                .querySelector('#garage')
+                .classList.replace('f-col', 'd-hidden')
+            document
+                .querySelector('#winners')
+                .classList.replace('d-hidden', 'f-col')
+        } else {
+            document
+                .querySelector('#winners')
+                .classList.replace('f-col', 'd-hidden')
+            document
+                .querySelector('#garage')
+                .classList.replace('d-hidden', 'f-col')
+        }
+    })
+
+    return toggler
+}
+
+export default setViewToggler
